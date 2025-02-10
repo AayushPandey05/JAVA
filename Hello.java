6defb1987b9f59644b1228f65a5d1c77abaf9e67
Aayush Pandey <139016137+AayushPandey05@users.noreply.github.com>
2025-02-10 22:23:12 +0530

    Basic Java

---
+++ Hello.java
@@ -0,0 +1,31 @@
+public class Hello
+{
+    public static void main(String a[])
+    {
+        // System.out.println("Hello World");
+        int num1 = 3;
+        int num2 = 2;
+        int result = num1 + num2;
+        // System.out.println(result);
+
+        //!..Primitive data types
+        int num = 2;
+        byte by = 127;
+        short sh = 558;
+        long l = 54787l; //we should enter L after the digit
+        
+
+        float f = 4.5f; //we should enter F after the digit because by default in java it is double
+        double d = 1.5;
+
+        char c = 'a';
+
+        boolean b = true;
+
+        //Literals
+
+        int mine = 0x7E;
+        int num11 = 0b101;
+        System.out.println(num11);
+    }
+}
---
+++ Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Input{
+    public static void main(String a[]){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num = scn.nextInt();
+        System.out.println(num);
+    }
+}
